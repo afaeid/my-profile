491943a2b8f55da76bd4bf44823e90baa518c41e
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-06-03 21:13:41 +0600

    ok

--- main.js
+++ main.js
@@ -59,41 +59,68 @@ var sectionMoreMemory = document.querySelector('.changeable-section4')
 skill.onclick = ()=>{
   skill.style.color = 'var(--style-font-color2)';
   experience.style.color = 'var(--style-font-color1)';
-  education.style.color = 'var(--style-font-color1)'
+  education.style.color = 'var(--style-font-color1)';
+  moreMemory.style.color = 'var(--style-font-color1)';
   
   skill.style.boxShadow = 'var(--outer-shadow)';
   experience.style.boxShadow = 'none';
   education.style.boxShadow = 'none';
+  moreMemory.style.boxShadow = 'none';
   
   sectionSkill.style.display = 'block';
   sectionExperience.style.display = 'none';
   sectionEducation.style.display = 'none';
+  sectionMoreMemory.style.display = 'none';
 }
 experience.onclick = ()=>{
   
   skill.style.color = 'var(--style-font-color1)';
   experience.style.color = 'var(--style-font-color2)';
   education.style.color = 'var(--style-font-color1)'
+  moreMemory.style.color = 'var(--style-font-color1)';
   
   skill.style.boxShadow = 'none';
   experience.style.boxShadow = 'var(--outer-shadow)';
   education.style.boxShadow = 'none';
+  moreMemory.style.boxShadow = 'none';
 
   sectionSkill.style.display = 'none';
   sectionExperience.style.display = 'block';
   sectionEducation.style.display = 'none';
+  sectionMoreMemory.style.display = 'none'
 }
 education.onclick = ()=>{
   
   skill.style.color = 'var(--style-font-color1)';
   experience.style.color = 'var(--style-font-color1)';
+  moreMemory.style.color = 'var(--style-font-color1)';
   education.style.color = 'var(--style-font-color2)'
   
   skill.style.boxShadow = 'none';
   experience.style.boxShadow = 'none';
   education.style.boxShadow = 'var(--outer-shadow)';
+  moreMemory.style.boxShadow = 'none';
 
   sectionSkill.style.display = 'none';
   sectionExperience.style.display = 'none';
   sectionEducation.style.display = 'block';
+  sectionMoreMemory.style.display = 'none';
+}
+moreMemory.onclick = ()=> {
+  
+  skill.style.color = 'var(--style-font-color1)';
+  experience.style.color = 'var(--style-font-color1)';
+  education.style.color = 'var(--style-font-color1)';
+  moreMemory.style.color = 'var(--style-font-color2)';
+  
+  skill.style.boxShadow = 'none';
+  experience.style.boxShadow = 'none';
+  education.style.boxShadow = 'none';
+  moreMemory.style.boxShadow = 'var(--outer-shadow)';
+  
+  sectionSkill.style.display = 'none';
+  sectionExperience.style.display = 'none';
+  sectionEducation.style.display = 'none';
+  sectionMoreMemory.style.display = 'block';
+  
 }
