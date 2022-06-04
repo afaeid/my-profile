d3ee721333a6466bc8f9574a3f259cf216cfd90e
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-06-04 13:17:12 +0600

    ok

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ menuCloseBtn.onclick = ()=>{
 var themeButton = document.querySelector('.theme-button');
 var themeIconMoon = document.querySelector('.theme-button .moon-icon')
 var themeIconSun = document.querySelector('.theme-button .sun-icon')
-
+/*
 themeButton.onclick = ()=>{
   document.body.classList.toggle('dark-theme')
   if(document.body.classList.contains('dark-theme')){
@@ -23,7 +23,25 @@ themeButton.onclick = ()=>{
     themeIconSun.style.display = 'none';
     themeIconMoon.style.display = 'inline-block';
   }
+}*/
+
+function darkTheme(){
+  localStorage.setItem('theme','dark');
+  localStorage.getItem('theme');
+  document.body.classList.add('dark-theme');
+  themeIconMoon.style.display = 'none';
+  themeIconSun.style.display = 'inline-block';
 }
+function lightTheme(){
+  document.body.classList.remove('dark-theme');
+  themeIconMoon.style.display = 'inline-block';
+  themeIconSun.style.display = 'none';
+  localStorage.removeItem('theme');
+}
+if(localStorage.getItem('theme')!== null){
+  darkTheme();
+}
+
 /*===== For the loader on profile Iamge ====== */
 var profImg = document.querySelector('.profile-img');
 var imgLoader = document.querySelector('.img-loader');
