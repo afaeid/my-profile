77ed5f1c8e9fc2c515f951ba4e2a8e5b9d5cbd51
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-06-07 18:12:57 +0600

    ok

--- js/sectionchange.js
+++ js/sectionchange.js
@@ -3,16 +3,19 @@
 var homePage = document.querySelector('.container-home');
 var aboutPage = document.querySelector('.about');
 var servicePage = document.querySelector('.service');
+var portfolioPage = document.querySelector('.portfolio')
 function openHomeSection(){
   
   localStorage.setItem('homePage','home');
   localStorage.getItem('homePage');
   localStorage.removeItem('aboutPage');
   localStorage.removeItem('servicePage');
+  localStorage.removeItem('portfolioPage');
   
   homePage.style.display = 'flex';
   aboutPage.style.display = 'none';
   servicePage.style.display = 'none';
+  portfolioPage.style.display = 'none';
 }
 if(localStorage.getItem('homePage') !== null){
   openHomeSection();
@@ -23,10 +26,12 @@ function openAboutSection(){
   localStorage.getItem('aboutPage');
   localStorage.removeItem('homePage');
   localStorage.removeItem('servicePage');
+  localStorage.removeItem('portfolioPage');
 
   homePage.style.display = 'none';
   aboutPage.style.display = 'flex';
   servicePage.style.display = 'none';
+  portfolioPage.style.display = 'none';
 }
 if(localStorage.getItem('aboutPage')!== null){
   openAboutSection();
@@ -36,12 +41,29 @@ function openServiceSection(){
   localStorage.setItem('servicePage','service');
   localStorage.getItem('servicePage');
   localStorage.removeItem('aboutPage');
-  localStorage.removeItem('homePage')
+  localStorage.removeItem('homePage');
+  localStorage.removeItem('portfolioPage');
   
   homePage.style.display = 'none';
   aboutPage.style.display = 'none';
   servicePage.style.display = 'flex';
+  portfolioPage.style.display = 'none';
 }
 if(localStorage.getItem('servicePage')!== null){
   openServiceSection();
+}
+function openPortfolioSection(){
+  localStorage.setItem('portfolioPage','portfolio');
+  localStorage.getItem('portfolioPage');
+  localStorage.removeItem('aboutPage');
+  localStorage.removeItem('homePage')
+  localStorage.removeItem('servicePage')
+  
+  homePage.style.display = 'none';
+  aboutPage.style.display = 'none';
+  servicePage.style.display = 'none';
+  portfolioPage.style.display = 'flex';
+}
+if(localStorage.getItem('portfolioPage')!== null){
+  openPortfolioSection();
 }
