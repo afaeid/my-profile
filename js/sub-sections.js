6dd945730f32566e0ef7e04d2dadd06ab5846af2
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-06-08 15:50:29 +0600

    ok

--- js/sub-sections.js
+++ js/sub-sections.js
@@ -78,3 +78,182 @@ moreMemory.onclick = ()=> {
   sectionMoreMemory.style.display = 'block';
   
 }
+/* Programe End*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Changing the title style of Portfolio section*/
+var all = document.querySelector('.portfolio .item-titles a:nth-child(1)');
+var webApplication = document.querySelector('.portfolio .item-titles a:nth-child(2)');
+var photoshop = document.querySelector('.portfolio .item-titles a:nth-child(3)');
+var mobileApps = document.querySelector('.portfolio .item-titles a:nth-child(4)');
+var eCommerce = document.querySelector('.portfolio .item-titles a:nth-child(5)');
+
+
+var pjWebApp = document.querySelectorAll('.project-web-app')
+var pjPhotoshop = document.querySelectorAll('.project-photoshop')
+var pjECommerce = document.querySelectorAll('.project-e-commerce')
+var pjMobileApp = document.querySelectorAll('.project-mobile-app');
+
+
+all.onclick = ()=>{
+  all.style.color = 'var(--style-font-color2)';
+  webApplication.style.color = 'var(--style-font-color1)';
+  photoshop.style.color = 'var(--style-font-color1)';
+  mobileApps.style.color = 'var(--style-font-color1)';
+  eCommerce.style.color = 'var(--style-font-color1)';
+
+  
+  all.style.boxShadow = 'var(--outer-shadow)';
+  webApplication.style.boxShadow = 'none';
+  photoshop.style.boxShadow = 'none';
+  mobileApps.style.boxShadow = 'none';
+  eCommerce.style.boxShadow = 'none';
+  
+  pjWebApp.forEach(section =>{
+    section.style.display = 'inline-block'
+  });
+  pjPhotoshop.forEach(section =>{
+    section.style.display = 'inline-block'
+  });
+  pjECommerce.forEach(section => {
+    section.style.display = 'inline-block'
+  });
+  pjMobileApp.forEach(section =>{
+    section.style.display = 'inline-block'
+  });
+  
+  
+}
+webApplication.onclick = ()=>{
+  all.style.color = 'var(--style-font-color1)';
+  webApplication.style.color = 'var(--style-font-color2)';
+  photoshop.style.color = 'var(--style-font-color1)';
+  mobileApps.style.color = 'var(--style-font-color1)';
+  eCommerce.style.color = 'var(--style-font-color1)';
+
+  
+  all.style.boxShadow = 'none';
+  webApplication.style.boxShadow = 'var(--outer-shadow)';
+  photoshop.style.boxShadow = 'none';
+  mobileApps.style.boxShadow = 'none';
+  eCommerce.style.boxShadow = 'none';
+  
+  pjWebApp.forEach(section =>{
+    section.style.display = 'inline-block'
+  });
+  pjPhotoshop.forEach(section =>{
+    section.style.display = 'none'
+  });
+  pjECommerce.forEach(section => {
+    section.style.display = 'none'
+  });
+  pjMobileApp.forEach(section =>{
+    section.style.display = 'none'
+  });
+  
+}
+photoshop.onclick = ()=>{
+  all.style.color = 'var(--style-font-color1)';
+  webApplication.style.color = 'var(--style-font-color1)';
+  photoshop.style.color = 'var(--style-font-color2)';
+  mobileApps.style.color = 'var(--style-font-color1)';
+  eCommerce.style.color = 'var(--style-font-color1)';
+
+  
+  all.style.boxShadow = 'none';
+  webApplication.style.boxShadow = 'none';
+  photoshop.style.boxShadow = 'var(--outer-shadow)';
+  mobileApps.style.boxShadow = 'none';
+  eCommerce.style.boxShadow = 'none';
+  
+  pjWebApp.forEach(section =>{
+    section.style.display = 'none'
+  });
+  pjPhotoshop.forEach(section =>{
+    section.style.display = 'inline-block'
+  });
+  pjECommerce.forEach(section => {
+    section.style.display = 'none'
+  });
+  pjMobileApp.forEach(section =>{
+    section.style.display = 'none'
+  });
+  
+  
+}
+mobileApps.onclick = ()=>{
+  all.style.color = 'var(--style-font-color1)';
+  webApplication.style.color = 'var(--style-font-color1)';
+  photoshop.style.color = 'var(--style-font-color1)';
+  mobileApps.style.color = 'var(--style-font-color2)';
+  eCommerce.style.color = 'var(--style-font-color1)';
+
+  
+  all.style.boxShadow = 'none';
+  webApplication.style.boxShadow = 'none';
+  photoshop.style.boxShadow = 'none';
+  mobileApps.style.boxShadow = 'var(--outer-shadow)';
+  eCommerce.style.boxShadow = 'none';
+  
+  pjWebApp.forEach(section =>{
+    section.style.display = 'none'
+  });
+  pjPhotoshop.forEach(section =>{
+    section.style.display = 'none'
+  });
+  pjECommerce.forEach(section => {
+    section.style.display = 'none'
+  });
+  pjMobileApp.forEach(section =>{
+    section.style.display = 'inline-block'
+  });
+  
+  
+}
+eCommerce.onclick = ()=>{
+  all.style.color = 'var(--style-font-color1)';
+  webApplication.style.color = 'var(--style-font-color1)';
+  photoshop.style.color = 'var(--style-font-color1)';
+  mobileApps.style.color = 'var(--style-font-color1)';
+  eCommerce.style.color = 'var(--style-font-color2)';
+
+  
+  all.style.boxShadow = 'none';
+  webApplication.style.boxShadow = 'none';
+  photoshop.style.boxShadow = 'none';
+  mobileApps.style.boxShadow = 'none';
+  eCommerce.style.boxShadow = 'var(--outer-shadow)';
+  
+  pjWebApp.forEach(section =>{
+    section.style.display = 'none'
+  });
+  pjPhotoshop.forEach(section =>{
+    section.style.display = 'none'
+  });
+  pjECommerce.forEach(section => {
+    section.style.display = 'inline-block'
+  });
+  pjMobileApp.forEach(section =>{
+    section.style.display = 'none'
+  });
+  
+  
+}
