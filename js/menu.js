e0c874ef55418e9b6ceb351021836559bf95750e
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-06-19 09:50:10 +0600

    ok

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,14 @@ var menuOpenBtn = document.querySelector('.menu-icon');
 var menuCloseBtn = document.querySelector('.menu-close-btn')
 var menu = document.querySelector('.nav-links');
 menuOpenBtn.onclick = ()=>{
+  menuOpenBtn.style.display = 'none';
+  menuCloseBtn.style.display = 'inline-block';
   menu.style.display = 'flex';
 }
 menuCloseBtn.onclick = ()=>{
-  menu.style.display = 'none';
+  menuOpenBtn.style.display = 'inline-block';
+  menuCloseBtn.style.display = 'none';
+  menu.style.display = 'none'
 }
 /* For closing the navbar when click any  page name */
 var navLi = document.querySelectorAll('.nav-links ul li');
