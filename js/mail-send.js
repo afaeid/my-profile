c9e5bf4f7deb525cf7a767f4369da0d642a813f1
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-06-25 06:50:15 +0600

    ok

--- js/mail-send.js
+++ js/mail-send.js
@@ -1,13 +1,13 @@
 var subBtn = document.querySelector('.mail-sender .submit button');
 var userName =  document.querySelector('.mail-sender .username');
 var userEmail = document.querySelector('.mail-sender .email')
-var userSebject = document.querySelector('.mail-sender .subject');
+var userSubject = document.querySelector('.mail-sender .subject');
 var userMsg = document.querySelector('.mail-sender textarea');
 
 subBtn.onclick = ()=>{
 userName.value = '';
 userEmail.value = '';
-userSebject.value = '';
+userSubject.value = '';
 userMsg.value = '';
 
 alert('Sorry! For security reasons we are currently disable to send messages from web. We are trying to fix it as soon as possible')
